5a07b5c6309f4c7663121787e676b4b6420e8dea
Thiago Oliveira <thiago@outlook.com.br>
2023-01-25 13:34:58 -0300

    Fix spacing between elements

--- DubDubGrub/DubDubGrub/ProfileView.swift
+++ DubDubGrub/DubDubGrub/ProfileView.swift
@@ -12,7 +12,7 @@ struct ProfileView: View {
     @State private var lastName     = ""
     @State private var companyName  = ""
     @State private var bio          = ""
-    
+
     var body: some View {
         VStack {
             ZStack {
@@ -20,10 +20,9 @@ struct ProfileView: View {
                     .frame(height: 130)
                     .cornerRadius(12)
                     .padding(.horizontal)
-                HStack {
+                HStack(spacing: 16) {
                     ZStack {
                         AvatarView(size: 84)
-                            .padding(.leading, 12)
                         Image(systemName: "square.and.pencil")
                             .resizable()
                             .scaledToFit()
@@ -32,7 +31,7 @@ struct ProfileView: View {
                             .offset(y: 30)
                     }
                     .padding(.leading, 12)
-                    
+
                     VStack(spacing: 1) {
                         TextField("First Name", text: $firstName)
                             .font(.system(size: 32, weight: .bold))
@@ -42,7 +41,7 @@ struct ProfileView: View {
                             .font(.system(size: 32, weight: .bold))
                             .lineLimit(1)
                             .minimumScaleFactor(0.75)
-                        
+
                         TextField("Company Name", text: $companyName)
                     }
                     .padding(.trailing, 16)
